090f0dd74fd461317d40d7cc91072a181b792504
tomfaical <142061917+tomfaical@users.noreply.github.com>
2023-09-06 12:15:25 -0300

    Update Programa X.py

--- Programa X.py
+++ Programa X.py
@@ -1,17 +1,28 @@
-# Programa X v0.1
+# Programa X v0.2
 
 # importando libs
+# time serve pra esperar pra limpar a tela
 import time
+# os serve pra limpar a tela
 import os
+# colorama serve pra colorir texto
 from colorama import init, Fore, Back
+# settando autoreset como True pra não pintar td o texto com a msm cor dps do comando
 init(autoreset=True)
 
 # declarando variáveis significativas
+# faz o programa rodar
 programRunning = True
+# lista pros nomes
 nome = []
+# lista pros cpf
 cpf = []
+# lista pros tipos sanguineos
 sangue = []
+# contadores
 r11 = False
+n1check = False
+n2check = False
 
 # começando o loop
 while programRunning:
@@ -24,7 +35,7 @@ while programRunning:
 
     # escolha 1
     if resposta1 == '1':
-        # retornando a escolha do usuário e limpando a tela
+        # retornando a escolha do usuário, esperando 5s e limpando a tela
         print("\nDigitado 1.")
         time.sleep(1.5)
         os.system('cls')
@@ -32,26 +43,59 @@ while programRunning:
         # cabeçalho
         print("Inserção de Dados")
 
-        n1 = str(input("- Digite o nome: "))
-        if n1.isalpha():
-            # adicionando o nome que o usuário digitou no último índice da lista
-            nome.append(n1)
-        else:
-            print("Entrada inválida.")
+        n1 = str(input("- Digite o primeiro nome: "))
+        # Loop até o usuário digitar um nome válido
+        while not n1check:
+            # formatando n1
+            n1 = n1.lower()
+            # atualizando o contador
+            n1check = True
+            # se o n1 é composto por letras
+            if n1.isalpha():
+                # Loop até o usuário digitar um sobrenome válido
+                while not n2check:
+                    n2 = str(input("- Digite o sobrenome: "))
+                    # se o n2 é composto por letras
+                    if n2.isalpha():
+                        # adicionando o nome que o usuário digitou no último índice da lista
+                        n2 = n2.lower()
+                        # n1 vira n1 com a 1a letra maiuscula, espaço, n2 com a 1a letra maiuscula
+                        n1 = n1.capitalize() + " " + n2.capitalize()
+                        # adicionando o n1 ao ultimo indice vazio da lista nome
+                        nome.append(n1)
+                        # atualizando o contador
+                        n2check = True
+                    else:
+                        print("Entrada inválida.")
+            else:
+                print("Entrada inválida.")
 
         n1 = str(input("- Digite o cpf: "))
-        if len(n1) == 11:
-            # adicionando o cpf que o usuário digitou no último índice da lista
-            cpf.append(n1)
-        else:
-            print("Entrada inválida.")
+        # Loop até o usuário digitar um CPF válido
+        while not n1check:
+            # se o n1 é composto por numeros
+            if n1.isnumeric():
+                # add n1 no último índice da lista
+                cpf.append(n1)
+                #atualizando o contador
+                n1check = True
+            else:
+                print("Entrada inválida.")
 
         n1 = str(input("- Digite o tipo sanguíneo: "))
-        if len(n1) == 2:
-            # adicionando o tipo sanguíneo que o usuário digitou no último índice da lista
-            sangue.append(n1)
-        else:
-            print("Entrada inválida.")
+
+        while not n1check:
+        # Loop até o usuário digitar um tipo sanguineo válido
+            # se n1 for letra ou numero
+            if n1.isalnum():
+                # formatando n1
+                n1 = n1.upper()
+                # add n1 no último índice da lista
+                sangue.append(n1)
+                #atualizando o contador
+                n1check = True
+            else:
+                print("Entrada inválida.")
 
         # atualizando o contador para marcar que já foram inseridos dados
         r11 = True
@@ -93,7 +137,21 @@ while programRunning:
         print("BUSCA NO BANCO DE DADOS")
 
         # recebendo o termo de busca
-        busca = input("Digite o termo de busca: ")
+        busca = str(input("Digite o termo de busca: "))
+
+        # arrumando a busca
+        # se busca não conter números (aka se busca for nome ou tipo sanguíneo), formatar
+        if not busca.isnumeric():
+            busca = busca.lower()
+            busca = busca.capitalize()
+
+        # se busca conter números (aka se busca for CPF), blz
+        elif busca.isnumeric():
+            pass
+
+        # se o usuário encontrar outra opção que não "número" ou "não número", busca inválida
+        else:
+            print("Busca inválida.")
 
         # loop para rodar (tamanho da lista nome) vezes
         for i in range(len(nome)):
