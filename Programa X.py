03dd518c3a06323dff8154b9c46a7f44b0528144
tomfaical <142061917+tomfaical@users.noreply.github.com>
2023-09-09 00:44:57 -0300

    Update Programa X.py

--- Programa X.py
+++ Programa X.py
@@ -1,4 +1,4 @@
-# Programa X v0.2
+# Programa X v1.0
 
 # importando libs
 # time serve pra esperar pra limpar a tela
@@ -6,7 +6,9 @@ import time
 # os serve pra limpar a tela
 import os
 # colorama serve pra colorir texto
+import colorama
 from colorama import init, Fore, Back
+colorama.init()
 # settando autoreset como True pra não pintar td o texto com a msm cor dps do comando
 init(autoreset=True)
 
@@ -27,9 +29,17 @@ n2check = False
 # começando o loop
 while programRunning:
     # Menu Inicial
-    print("\n Programa X \n\n 1 - Entrada de Dados \n 2 - Mostrar conjunto de dados"
-          "\n 3 - Procurar no conjunto de dados \n 4 - Sair \n")
-
+    print(Back.MAGENTA + Fore.BLACK + 'Programa X'.center(40))
+    
+    print(Fore.GREEN + '1- Entrada de Dados'.center(30))
+    
+    print(Fore.GREEN + '2- Mostrar conjunto de dados '.center(40))
+    
+    print(Fore.GREEN + '3- Procurar no conjunto de dados '.center(40))
+    
+    print(Fore.GREEN + '4- Sair'.center(30))
+          
+    
     # Tomando a escolha do usuário
     resposta1 = str(input("Digite o número da opção: "))
 
@@ -41,9 +51,9 @@ while programRunning:
         os.system('cls')
 
         # cabeçalho
-        print("Inserção de Dados")
+        print(Back.MAGENTA + Fore.BLACK + "Inserção de Dados".center(40))
 
-        n1 = str(input("- Digite o primeiro nome: "))
+        n1 = str(input(Fore.GREEN + "- Digite o primeiro nome: "))
         # Loop até o usuário digitar um nome válido
         while not n1check:
             # formatando n1
@@ -54,7 +64,7 @@ while programRunning:
             if n1.isalpha():
                 # Loop até o usuário digitar um sobrenome válido
                 while not n2check:
-                    n2 = str(input("- Digite o sobrenome: "))
+                    n2 = str(input(Fore.GREEN + "- Digite o sobrenome: "))
                     # se o n2 é composto por letras
                     if n2.isalpha():
                         # adicionando o nome que o usuário digitou no último índice da lista
@@ -70,7 +80,7 @@ while programRunning:
             else:
                 print("Entrada inválida.")
 
-        n1 = str(input("- Digite o cpf: "))
+        n1 = str(input(Fore.GREEN + "- Digite o cpf: "))
         # Loop até o usuário digitar um CPF válido
         while not n1check:
             # se o n1 é composto por numeros
@@ -82,7 +92,7 @@ while programRunning:
             else:
                 print("Entrada inválida.")
 
-        n1 = str(input("- Digite o tipo sanguíneo: "))
+        n1 = str(input(Fore.GREEN + "- Digite o tipo sanguíneo: "))
 
         while not n1check:
         # Loop até o usuário digitar um tipo sanguineo válido
@@ -100,10 +110,10 @@ while programRunning:
         # atualizando o contador para marcar que já foram inseridos dados
         r11 = True
 
-        print("Dados inseridos com sucesso!")
+        print(Fore.GREEN + "Dados inseridos com sucesso!")
 
         # esperando interação do usuário e voltando para o menu inicial
-        voltar = input("Aperte ENTER para continuar...")
+        voltar = input(Fore.GREEN + "Aperte ENTER para continuar...")
         os.system('cls')
 
     # escolha 2 e passou pela 1
@@ -114,16 +124,16 @@ while programRunning:
         os.system('cls')
 
         # cabeçalho
-        print("BANCO DE DADOS \n")
+        print(Back.MAGENTA + Fore.BLACK +"BANCO DE DADOS \n".center(40))
         print("-" * 20)
-        print("CPF      TIPO SANGUÍNEO      NOME")
+        print(Fore.GREEN + "CPF      TIPO SANGUÍNEO      NOME")
         # loop para rodar (tamanho da lista nome) vezes
         for i in range(len(nome)):
             # retornando cada uma das entradas
             print(f"{cpf[i]}     {sangue[i]}     {nome[i]}")
         print("-" * 20)
         # esperando interação do usuário e voltando para o menu inicial
-        voltar = input("Aperte ENTER para continuar...")
+        voltar = input(Fore.GREEN + "Aperte ENTER para continuar...")
         os.system('cls')
 
     # escolha 3 e passou pela 1
@@ -134,10 +144,10 @@ while programRunning:
         os.system('cls')
 
         # cabeçalho
-        print("BUSCA NO BANCO DE DADOS")
+        print(Back.MAGENTA + Fore.BLACK + "BUSCA NO BANCO DE DADOS".center(40))
 
         # recebendo o termo de busca
-        busca = str(input("Digite o termo de busca: "))
+        busca = str(input(Fore.GREEN + "Digite o termo de busca: "))
 
         # arrumando a busca
         # se busca não conter números (aka se busca for nome ou tipo sanguíneo), formatar
